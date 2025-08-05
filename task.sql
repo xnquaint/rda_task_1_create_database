300067e443bb863cf1d2184bfa24c640d8ad9e6e
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2025-08-05 21:02:29 +0300

    fix

--- task.sql
+++ task.sql
@@ -3,35 +3,35 @@ USE ShopDB;
 
 CREATE TABLE Products (
 	ID INT AUTO_INCREMENT,
-    Name VARCHAR(50),
-    Description VARCHAR(100),
-    Price INT,
-    WarehouseAmount INT,
-    PRIMARY KEY (ID)
+  Name VARCHAR(50),
+  Description VARCHAR(100),
+  Price INT,
+  WarehouseAmount INT,
+  PRIMARY KEY (ID)
 );
 
 CREATE TABLE Customers (
 	ID INT AUTO_INCREMENT,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(100),
-    Email VARCHAR(50),
-    Address VARCHAR(100),
-    PRIMARY KEY (ID)
+  FirstName VARCHAR(50),
+  LastName VARCHAR(100),
+  Email VARCHAR(50),
+  Address VARCHAR(100),
+  PRIMARY KEY (ID)
 );
 
 CREATE TABLE Orders (
 	ID INT AUTO_INCREMENT,
-	CustomerID INT,
-    Date DATE,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL
+	CustomerID INT NULL,
+  Date DATE,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL
 );
 
 CREATE TABLE OrderItems (
 	ID INT AUTO_INCREMENT,
-	OrderID INT,
-	ProductID INT,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
-    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
+	OrderID INT NULL,
+	ProductID INT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
+  FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
 );
